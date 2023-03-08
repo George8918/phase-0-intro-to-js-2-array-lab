d714a31bd4d77f32f86f812c15dce669ebbadfb1
George Zheng <GZheng8918@gmail.com>
2023-03-07 23:10:28 -0800

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,39 @@
 require ( './helpers.js' );
 
+const cats=["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+
+function appendCat(name) {
+  return [...cats,name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat(name) {
+  return cats.slice(0,-1);
+}
+
+function removeFirstCat(name) {
+  return cats.slice(1);
+}
+
 describe('index.js', function () {
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
